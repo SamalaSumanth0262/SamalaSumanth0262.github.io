fc71c9dd3d1b2005be6046f85d9333f8f5350a19
Samala Sumanth <sumanth.samala@letsventure.com>
2019-12-16 16:21:30 +0530

    inetermedaiate

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1> Comming Soon.... </h1>
+        <h1>Comming Soon.... </h1>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -20,14 +20,14 @@ module.exports = {
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/", //BUNDLED OUTPUT WILL BE SERVED FROM HERE
+    path: path.resolve(__dirname, "build/"),
+    publicPath: "/build/", //BUNDLED OUTPUT WILL BE SERVED FROM HERE
     filename: "bundle.js"
   },
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
+    publicPath: "http://localhost:3000/build/",
     hotOnly: true
   },
   plugins: [new webpack.HotModuleReplacementPlugin()]
