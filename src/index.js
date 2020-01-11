2176397b7972053e2d9d8d09dce4f9df2fb5f3e1
Samala Sumanth <sumanth.samala@letsventure.com>
2020-01-11 19:25:06 +0530

    deleted React Ap

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "../assets/styles/global.scss";
-import App from "./App.js";
-ReactDOM.render(<App />, document.getElementById("root"));
